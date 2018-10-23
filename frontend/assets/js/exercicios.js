682fd7eedb3de258285e1bea141518ee9618a0bf
biancaazuma <biancaazuma@gmail.com>
2018-10-23 00:29:28 -0300

    QuizJs

--- frontend/assets/js/exercicios.js
+++ frontend/assets/js/exercicios.js
@@ -1,3 +1,16 @@
 function fecharModal(){
    $("#myModal").modal("hide");
 };
+count = 1;
+$(document).ready(function(){
+});
+
+function prox(){
+    $("#q"+count).addClass("escondido");
+    count++;
+    $("#q"+count).removeClass("escondido"); 
+};
+
+function fim(){
+    alert("acabou");
+};
